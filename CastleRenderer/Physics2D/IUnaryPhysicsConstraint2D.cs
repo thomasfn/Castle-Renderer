b550f1555eacb59d5b9c9feaa73388bfb0e2bff8
thomasfn <thomasfn@fortfn.co.uk>
2015-03-09 11:39:05 +0000

    Added missing interface

---
+++ CastleRenderer/Physics2D/IUnaryPhysicsConstraint2D.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using SlimDX;
+
+namespace CastleRenderer.Physics2D
+{
+    /// <summary>
+    /// Represents a constraint between two physics objects
+    /// </summary>
+    public interface IUnaryPhysicsConstraint2D : IPhysicsConstraint2D
+    {
+        /// <summary>
+        /// Gets the first object constrained by this constraint
+        /// </summary>
+        IPhysicsObject2D ObjectA { get; }
+
+        /// <summary>
+        /// Gets the position of the first attachment point in model space of ObjectA
+        /// </summary>
+        Vector2 PositionA { get; }
+    }
+}
